75791e4e68864554f94577ec656ac3f78a35afec
swaggytt <ichromex@hotmail.com>
2021-02-07 13:48:27 +0700

    create delete API

--- backend/routes.js
+++ backend/routes.js
@@ -7,16 +7,26 @@ router.get("/store", async (req, res) => {
   res.send("Welcome to my Store");
 });
 
-router.get("/store/products",async (req, res) => {
-    const product = await ProductModel.find();
-    res.json(product);
-  });
+router.get("/store/products", async (req, res) => {
+  const product = await ProductModel.find();
+  res.json(product);
+});
 
 router.post("/store/products", async (req, res) => {
   const product = new ProductModel(req.body);
   await product.save();
   res.send(product);
   res.status(201);
+});
+
+router.delete("/store/products/:id", async (req, res) => {
+  try {
+    await ProductModel.deleteOne({ _id: req.params.id });
+    res.sendStatus(204);
+  } catch {
+    res.sendStatus(404);
+    res.send({ error: "Product does not exist" });
+  }
 });
 
 module.exports = router;
